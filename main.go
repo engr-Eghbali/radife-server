e28190a84d6af26bbe4cef5aa206f3d8557a8643
reza-eghbali <engr.eghbali@gmail.com>
2018-04-01 14:30:18 +0430

    fetch goods in progress

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func category_ctrl(w http.ResponseWriter, r *http.Request) {
 				i++
 			}
 
-			response = response + "<div class=\"market\" onclick=\"getgoods()\"><img class=\"marketicon\" src=\"" + shop.Avatar + "\"><p class=\"restname\">" + shop.Name + "</p><p class=\"restlocation\"><span class=\"glyphicon glyphicon-pushpin\"></span>" + shop.Add + "</p><div id=\"reststatus\"><p class=\"stars\">" + ranking + "</p><p class=\"restoff\">" + offer + "</p><p class=\"bike\"><i class=\"fas fa-motorcycle\"></i>" + delivery + "</p></div></div>"
+			response = response + "<div class=\"market\" onclick=\"getgoods(\"" + shop.Phone + "\")><img class=\"marketicon\" src=\"" + shop.Avatar + "\"><p class=\"restname\">" + shop.Name + "</p><p class=\"restlocation\"><span class=\"glyphicon glyphicon-pushpin\"></span>" + shop.Add + "</p><div id=\"reststatus\"><p class=\"stars\">" + ranking + "</p><p class=\"restoff\">" + offer + "</p><p class=\"bike\"><i class=\"fas fa-motorcycle\"></i>" + delivery + "</p></div></div>"
 
 		}
 		fmt.Fprintf(w, response)
@@ -128,6 +128,36 @@ func category_ctrl(w http.ResponseWriter, r *http.Request) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////get goods of each shop//////////////////////////////
+
+func category_ctrl(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/javascript")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method == "POST" {
+		var response string
+		var goods []req.Good
+		var ranking string
+		var i int64 = 0
+
+		r.ParseForm()
+
+		shopID := r.Form["shopID"][0]
+		category := r.Form["cat"][0]
+		goods = req.Get_goods(shopID, category)
+
+		for _, good := range goods {
+
+			response = response + "<div class=\"good\"><img id=\"goodicon" + good.ID + "\"class=\"goodicon\" src=\"" + good.Pic + "\"><div class=\"goodinfo\"></br></br><p1 class=\"goodname\" id=\"goodname" + good.ID + "\">" + good.Name + "\"</p1></br><p2 class=\"gooddetail\" id=\"gooddetail" + good.ID + "\">" + good.Info + "</p2></br><p3 class=\"goodprice\" id=\"goodprice" + good.ID + "\">" + good.Price + "تومان</p3></div><div id=\"addremove\"><button class=\"adder\" onclick=\"addgood('" + good.ID + "')\"></button><button id=\"remover" + good.ID + "\" class=\"remover\" onclick=\"removegood('" + good.ID + "')\"></button></div></div>"
+
+		}
+		fmt.Fprintf(w, response)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
 
 func main() {
 
@@ -201,13 +231,37 @@ func main() {
 		IsDevelopment: true, // This will cause the AllowedHosts, SSLRedirect, and STSSeconds/STSIncludeSubdomains options to be ignored during development. When deploying to production, be sure to set this to false.
 	})
 
+	goods_rout := secure.New(secure.Options{
+		AllowedHosts:            []string{"ssl.example.com"},                     // AllowedHosts is a list of fully qualified domain names that are allowed. Default is empty list, which allows any and all host names.
+		HostsProxyHeaders:       []string{"X-Forwarded-Hosts"},                   // HostsProxyHeaders is a set of header keys that may hold a proxied hostname value for the request.
+		SSLRedirect:             true,                                            // If SSLRedirect is set to true, then only allow HTTPS requests. Default is false.
+		SSLTemporaryRedirect:    false,                                           // If SSLTemporaryRedirect is true, the a 302 will be used while redirecting. Default is false (301).
+		SSLHost:                 "ssl.example.com",                               // SSLHost is the host name that is used to redirect HTTP requests to HTTPS. Default is "", which indicates to use the same host.
+		SSLProxyHeaders:         map[string]string{"X-Forwarded-Proto": "https"}, // SSLProxyHeaders is set of header keys with associated values that would indicate a valid HTTPS request. Useful when using Nginx: `map[string]string{"X-Forwarded-Proto": "https"}`. Default is blank map.
+		STSSeconds:              315360000,                                       // STSSeconds is the max-age of the Strict-Transport-Security header. Default is 0, which would NOT include the header.
+		STSIncludeSubdomains:    true,                                            // If STSIncludeSubdomains is set to true, the `includeSubdomains` will be appended to the Strict-Transport-Security header. Default is false.
+		STSPreload:              true,                                            // If STSPreload is set to true, the `preload` flag will be appended to the Strict-Transport-Security header. Default is false.
+		ForceSTSHeader:          false,                                           // STS header is only included when the connection is HTTPS. If you want to force it to always be added, set to true. `IsDevelopment` still overrides this. Default is false.
+		FrameDeny:               true,                                            // If FrameDeny is set to true, adds the X-Frame-Options header with the value of `DENY`. Default is false.
+		CustomFrameOptionsValue: "SAMEORIGIN",                                    // CustomFrameOptionsValue allows the X-Frame-Options header value to be set with a custom value. This overrides the FrameDeny option. Default is "".
+		ContentTypeNosniff:      true,                                            // If ContentTypeNosniff is true, adds the X-Content-Type-Options header with the value `nosniff`. Default is false.
+		BrowserXssFilter:        true,                                            // If BrowserXssFilter is true, adds the X-XSS-Protection header with the value `1; mode=block`. Default is false.
+		CustomBrowserXssValue:   "1; report=https://example.com/xss-report",      // CustomBrowserXssValue allows the X-XSS-Protection header value to be set with a custom value. This overrides the BrowserXssFilter option. Default is "".
+		ContentSecurityPolicy:   "default-src 'self'",                            // ContentSecurityPolicy allows the Content-Security-Policy header value to be set with a custom value. Default is "".
+		// PublicKey: `pin-sha256="base64+primary=="; pin-sha256="base64+backup=="; max-age=5184000; includeSubdomains; report-uri="https://www.example.com/hpkp-report"`, // PublicKey implements HPKP to prevent MITM attacks with forged certificates. Default is "".
+		//ReferrerPolicy: "same-origin" // ReferrerPolicy allows the Referrer-Policy header with the value to be set with a custom value. Default is "".
+		//***triger***
+		IsDevelopment: true, // This will cause the AllowedHosts, SSLRedirect, and STSSeconds/STSIncludeSubdomains options to be ignored during development. When deploying to production, be sure to set this to false.
+	})
+
 	app1 := submit_rout.Handler(http.HandlerFunc(submit_ctrl))
 	app2 := setAddress_rout.Handler(http.HandlerFunc(setAddress_ctrl))
 	app3 := category_rout.Handler(http.HandlerFunc(category_ctrl))
+	app4 := goods_rout.Handler(http.HandlerFunc(goods_ctrl))
 	http.Handle("/submit", app1)
 	http.Handle("/setAddress", app2)
 	http.Handle("/category", app3)
-
+	http.Handle("/goods", app4)
 	log.Fatal(http.ListenAndServe(":80", nil))
 
 }
--- packages/req/req.go
+++ packages/req/req.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	mgo "gopkg.in/mgo.v2"
+	"mgo.v2/bson"
 )
 
+//////////////////////preview of each shop/////////////////////////
 type PreShop struct {
 	Name string `json:"name"`
 
 	Add string `json:"add"`
 
-	Stars int64 `json:"stars"`
+	Phone string `json:"phone"`
+
+	Stars int64 `json:"star"`
 
 	Avatar string `json:"avatar"`
 
@@ -20,6 +24,21 @@ type PreShop struct {
 	Delivery int64 `json:"delivery"`
 }
 
+////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////each good info////////////////////
+
+type Good struct {
+	ID    bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Name  string        `json:"name"`
+	Price int64         `json:"price"`
+	Pic   string        `json:"pic"`
+	Info  string        `json:"detail"`
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////get shops in a category//////////////////////
 func Get_category(cat string) (preview []PreShop) {
 
 	var results []PreShop
@@ -37,6 +56,40 @@ func Get_category(cat string) (preview []PreShop) {
 	c := session.DB("shopinfo").C(cat)
 
 	err = c.Find(nil).All(&results)
+
+	if err != nil {
+
+		log.Print("\n category query failed:\n")
+		log.Print(err)
+		return nil
+
+	} else {
+		return results
+
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////get goods of each shop/////////////////////////////
+func Get_goods(shopid string, cat string) (goods []Good) {
+
+	var results []Good
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+
+		log.Print("\n!!!!-- DB connection error:")
+		log.Print(err)
+		log.Print("\n")
+		return nil
+	}
+
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("goods").C(cat)
+
+	err = c.Find(bson.M{"shopid": shopid}).All(&results)
 
 	if err != nil {
 
