efc46e33ef5612e0e16491e637f178f456751502
reza-eghbali <engr.eghbali@gmail.com>
2018-04-01 23:52:08 +0430

    order cart in progress(befor test)

--- packages/req/req.go
+++ packages/req/req.go
@@ -2,10 +2,17 @@ package magic
 
 import (
 	"log"
+	"strings"
+	"time"
 
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
+type coord struct {
+	x string `json:"x"`
+	y string `json:"y"`
+}
 
 //////////////////////preview of each shop/////////////////////////
 type PreShop struct {
@@ -37,6 +44,22 @@ type Good struct {
 }
 
 ///////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////each cart info////////////////////////
+type Order struct {
+	Customer    string   `json:"customer"`
+	Courier     string   `json:"courier"`
+	Cart        []string `json:"cart"`
+	Total       int64    `json:"total"`
+	DateIn      string   `json:"date-in"`
+	TimeIn      string   `json:"time-in"`
+	DateOut     string   `json:"date-out"`
+	TimeOut     string   `json:"time-out"`
+	Origin      coord    `json:"origin"`
+	Destination coord    `json:"destination"`
+	Recieved    int32    `json:"recieved"`
+	Pay         int32    `json:"pay"`
+}
 
 //////////////////////////////get shops in a category//////////////////////
 func Get_category(cat string) (preview []PreShop) {
@@ -102,4 +125,49 @@ func Get_goods(shopid string, cat string) (goods []Good) {
 
 	}
 
+}
+
+////////////////////////////////////////////////////////////////////
+
+//////////////////////////set order/////////////////////////////////
+
+func Send_cart(shopID string, customer string, x string, y string, add string, total string, cart string) (flg bool) {
+
+	var result Order
+	var origin coord
+	var destination coord
+
+	order_array := strings.Split(cart, "#")
+	origin.x = "0"
+	origin.y = "0"
+	destination.x = x
+	destination.y = y
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+
+		log.Print("\n!!!!-- DB connection error:")
+		log.Print(err)
+		log.Print("\n")
+		return false
+	} else {
+
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB("orderinfo").C("order")
+
+		err = c.Insert(&Order{Customer: customer, Courier: "0", Cart: order_array, Total: total, DateIn: time.Now().Local().Format("2006-01-02"), TimeIn: time.Now().Format("3:04PM"), DateOut: "0", TimeOut: "0", Origin: origin, Destination: destination, Recieved: 0, Pay: 0})
+
+		if err != nil {
+
+			log.Print("\n !!!!!!!!! new order failed by:" + customer + "!!!!!!!!\n")
+			return false
+
+		} else {
+
+			log.Print("\nnew order submited by:" + customer + "\n")
+			return true
+		}
+
+	}
 }
